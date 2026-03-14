ba97deafbf4ba1053b8bb6b6f2e9ad1ac529c3cd
M. Hikari Reiziq Rakhadinta <reiziqzip@gmail.com>
2026-03-14 18:41:02 +0700

    Bikin endpoint get by id dan POST

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const students = [
   { id: 2, name: 'Hanif', major: 'Sistem Informasi', university: 'ITB' },
   { id: 3, name: 'Zaki', major: 'Hukum', university: 'UI' }
 ];
-
+// 1. Endpoint GET 
 app.get('/students', (req, res) => {
   res.status(200).json({
     message: 'Berhasil mengambil semua data mahasiswa',
@@ -19,6 +19,51 @@ app.get('/students', (req, res) => {
 
 app.get('/', (req, res) => {
   res.status(200).send('Semoga Integrasi Sistem A ges yak');
+});
+
+// 2. Endpoint GET by Id
+app.get('/students/:id', (req, res) => {
+  const studentId = parseInt(req.params.id);
+  
+  const student = students.find(s => s.id === studentId);
+
+  if (student) {
+    res.status(200).json({
+      message: 'Data mahasiswa ditemukan',
+      data: student
+    });
+  } else {
+    res.status(404).json({
+      message: 'Mahasiswa tidak ditemukan'
+    });
+  }
+});
+
+// 3. Endpoint POST
+app.post('/students', (req, res) => {
+  const { name, major, university } = req.body;
+
+  if (!name || !major || !university) {
+    return res.status(400).json({
+      message: 'Nama, jurusan, dan universitas harus diisi!'
+    });
+  }
+
+  const newId = students.length > 0 ? students[students.length - 1].id + 1 : 1;
+
+  const newStudent = {
+    id: newId,
+    name: name,
+    major: major,
+    university: university
+  };
+
+  students.push(newStudent);
+
+  res.status(201).json({
+    message: 'Data mahasiswa berhasil ditambahkan',
+    data: newStudent
+  });
 });
 
 app.listen(port, () => {
